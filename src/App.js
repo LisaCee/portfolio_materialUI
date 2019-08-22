4acf5407adea4dea3fa9846e51178d7dafe68546
LisaCee <lisaceedesign@gmail.com>
2019-08-22 13:03:12 -0700

    app bar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import NavBar from './Components/NavBar';
+
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <CssBaseline/>
+      <NavBar />
     </div>
   );
 }
---
+++ src/Components/NavBar.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import {makeStyles} from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+
+const useStyles = makeStyles(theme => ({
+    root: {
+        flexGrow: 1,
+    },
+    menuButton: {
+        marginRight: theme.spacing(2),
+    }
+}));
+
+export default function NavBar() {
+    const classes = useStyles();
+
+    return (
+        <div className={classes.root}>
+            <AppBar>
+                <Toolbar>
+                    <IconButton edge='start' className={classes.menuButton} aria-label='menu'>
+                        <MenuIcon/>
+                    </IconButton>
+                    <Typography variant='h6' >
+                        Lisa Canini
+                    </Typography>
+                    <Typography variant='p' >
+                        Full Stack Web Developer
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+        </div>
+    )
+}
